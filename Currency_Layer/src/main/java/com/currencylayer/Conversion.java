b848dc77849490cca52228485d852a06272f38fa
Mario maio <57944979+mandro98@users.noreply.github.com>
2021-12-18 19:57:42 +0100

    converion method updated

--- Currency_Layer/src/main/java/com/currencylayer/Conversion.java
+++ Currency_Layer/src/main/java/com/currencylayer/Conversion.java
@@ -4,11 +4,14 @@ import java.io.IOException;
 import java.net.MalformedURLException;
 import java.net.URISyntaxException;
 import java.time.Instant;
+import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.ZoneId;
 import java.time.format.DateTimeFormatter;
 import java.util.Date;
+import java.util.LinkedHashMap;
 import java.util.Locale;
+import java.util.Map;
 
 import org.json.JSONObject;
 
@@ -104,17 +107,52 @@ public class Conversion {
 			 DateTimeFormatter formatter = DateTimeFormatter.
 		                ofPattern("yyyy-MM-dd",Locale.ITALY).withZone(ZoneId.of("Europe/Rome"));
 			String date=dat.format(formatter);
-			 
+			
 	            this.date=date ;
-			
-			
-			
-
-
-		
 		
 	}
-
+	public Map conversion(String src,String tgt) {
+		this.amount=1;
+		JSONParser a=new JSONParser();
+		a.saveOnFile("live.json", 1, LocalDate.now());
+		try {
+			this.src=a.getCurrencyfromFile("live.json", src.toUpperCase());
+		} catch (MalformedURLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (URISyntaxException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		try {
+			this.tgt=a.getCurrencyfromFile("live.json", tgt.toUpperCase());
+		} catch (MalformedURLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (URISyntaxException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+			double rateUSDx =this.tgt.getExchange_rate();
+			double rateUSDy =1/this.src.getExchange_rate();
+			this.exchange_rate_src_tgt=rateUSDx*rateUSDy;
+			Map <String, Object> map = new LinkedHashMap<String,Object>();
+			Currency currencyA=new Currency(this.src.getCode(), this.src.getDescription());
+			Currency currencyB = new Currency(this.tgt.getCode(), this.tgt.getDescription());
+			map.put("currency1", currencyA);
+			map.put("currency2", currencyB);
+			map.put("exchange_rate", this.exchange_rate_src_tgt);
+			return map;
+			
+	}
+	
+	
 	public double getExchange_rate_src_tgt() {
 		return exchange_rate_src_tgt;
 	}
