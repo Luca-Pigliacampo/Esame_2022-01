f562922ad631a2dbeb5bf9fb865b3407f4e71e10
Mario maio <57944979+mandro98@users.noreply.github.com>
2021-12-18 08:34:01 +0100

    LiveExchange root implemented (with USD only)

--- Currency_Layer/src/main/java/com/currencylayer/controller/Controller.java
+++ Currency_Layer/src/main/java/com/currencylayer/controller/Controller.java
@@ -1,26 +1,34 @@
 package com.currencylayer.controller;
 
+import java.time.LocalDate;
+
+import org.json.JSONObject;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.currencylayer.Conversion;
+import com.currencylayer.Pair;
+import com.currencylayer.parse.JSONParser;
 
 @RestController
 public class Controller {
-	
+
 	@GetMapping(value="/conversion", params= {"src","tgt","amount"})
 	public ResponseEntity<Conversion[]> getvalue(@RequestParam String[] src, @RequestParam String[] tgt,@RequestParam double amount) {
 		Conversion[] conv=new Conversion[src.length*tgt.length];
 		int k=0;
+		
 		for(int i=0;i<src.length;i++) {
 			for(int j=0;j<tgt.length;j++) {
-				 
-				 conv[k]=new Conversion();
-				 conv[k].conversion(src[i], tgt[j], amount);
-				 k=k+1;
-				 try {
+
+				
+				conv[k]=new Conversion();
+				conv[k].conversion(src[i], tgt[j], amount);
+				k=k+1;
+				try {
 					Thread.sleep(1000);
 				} catch (InterruptedException e) {
 					// TODO Auto-generated catch block
@@ -28,9 +36,17 @@ public class Controller {
 				}
 			}
 		}
-		
+
 		return ResponseEntity.ok(conv);	
 
-		}
+	}
+	@GetMapping (value="/liveExchange", params={"src"})
+	public ResponseEntity<Pair> getLivePair (@RequestParam String src){
+		JSONParser pairObj = new JSONParser();
+		Pair pair=pairObj.getPairfromApi(src, LocalDate.now());
+		
+		return ResponseEntity.ok(pair);
+		
+	}
 
 }
