d4bc01c41ca15743fbd2f7e374182e4e72f96d11
Mario maio <57944979+mandro98@users.noreply.github.com>
2021-12-20 15:35:55 +0100

    Creating exception model

--- Currency_Layer/src/main/java/com/currencylayer/exception/ExceptionModel.java
+++ Currency_Layer/src/main/java/com/currencylayer/exception/ExceptionModel.java
@@ -1,5 +1,41 @@
 package com.currencylayer.exception;
 
+import java.time.ZonedDateTime;
+
+import org.springframework.http.HttpStatus;
+
 public class ExceptionModel {
+	
+	private final String message;
+	private final HttpStatus httpStat;
+	private final ZonedDateTime time;
+	
+	public ExceptionModel (String message, HttpStatus httpStat, ZonedDateTime time) {
+		this.message=message;
+		this.httpStat=httpStat;
+		this.time=time;
+	}
+
+	/**
+	 * @return the message
+	 */
+	public String getMessage() {
+		return message;
+	}
+
+	/**
+	 * @return the httpStat
+	 */
+	public HttpStatus getHttpStat() {
+		return httpStat;
+	}
+
+	/**
+	 * @return the time
+	 */
+	public ZonedDateTime getTime() {
+		return time;
+	}
+
 
 }
