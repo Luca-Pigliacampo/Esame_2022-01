f2e0a358b3c67181b9a489af1e88e8dc34e1c7d6
Mario maio <57944979+mandro98@users.noreply.github.com>
2021-12-20 12:34:51 +0100

    Handler updated

--- Currency_Layer/src/main/java/com/currencylayer/exception/Handler.java
+++ Currency_Layer/src/main/java/com/currencylayer/exception/Handler.java
@@ -1,5 +1,31 @@
 package com.currencylayer.exception;
 
+import java.time.ZonedDateTime;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
 public class Handler {
+	
+	@ExceptionHandler(value = {CurrencyNotFoundException.class})
+	public ResponseEntity<ExceptionModel> handleCurrencyNotFoundException(CurrencyNotFoundException e){
+		
+		ExceptionModel er=new ExceptionModel (e.getMessage(), HttpStatus.BAD_GATEWAY, ZonedDateTime.now());
+
+		return new ResponseEntity<>(er, HttpStatus.BAD_GATEWAY);
+
+	}
+	
+	@ExceptionHandler(value = {DateErrorException.class})
+	public ResponseEntity<ExceptionModel> handleDateErrorException(DateErrorException e){
+		HttpStatus badRequest=HttpStatus.BAD_REQUEST;
+		ExceptionModel er=new ExceptionModel (e.getMessage(),badRequest, ZonedDateTime.now());
+		
+		return new ResponseEntity<>(er, badRequest);
+		
+	}
 
 }
