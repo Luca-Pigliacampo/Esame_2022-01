95f5eb449c17b07379f274b704445eac6c4840e4
Mario maio <57944979+mandro98@users.noreply.github.com>
2021-12-20 15:34:28 +0100

    Handler update with first three exceptions

--- Currency_Layer/src/main/java/com/currencylayer/exception/Handler.java
+++ Currency_Layer/src/main/java/com/currencylayer/exception/Handler.java
@@ -1,5 +1,38 @@
 package com.currencylayer.exception;
+import java.time.ZonedDateTime;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
 public class Handler {
+  
+    @ExceptionHandler(value = {CurrencyNotFoundException.class})
+	public ResponseEntity<ExceptionModel> handleCurrencyNotFoundException(CurrencyNotFoundException e){
 
+		ExceptionModel er=new ExceptionModel (e.getMessage(), HttpStatus.BAD_REQUEST, ZonedDateTime.now());
+
+		return new ResponseEntity<>(er, HttpStatus.BAD_REQUEST);
+
+	}
+
+	@ExceptionHandler(value = {DateErrorException.class})
+	public ResponseEntity<ExceptionModel> handleDateErrorExceptionException(CurrencyNotFoundException e){
+
+		ExceptionModel er=new ExceptionModel (e.getMessage(), HttpStatus.BAD_REQUEST, ZonedDateTime.now());
+
+		return new ResponseEntity<>(er, HttpStatus.BAD_REQUEST);
+
+	}
+
+	@ExceptionHandler (value= {AmountFormatException.class})
+	public ResponseEntity<ExceptionModel> handleAmountFormaatException(CurrencyNotFoundException e){
+
+		ExceptionModel er=new ExceptionModel (e.getMessage(), HttpStatus.BAD_REQUEST, ZonedDateTime.now());
+
+		return new ResponseEntity<>(er, HttpStatus.BAD_REQUEST);
+
+	}
 }
