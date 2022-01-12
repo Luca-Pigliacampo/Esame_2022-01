3ba37dcb4671bdad1b95f011f892a596d1ae65b1
Mario maio <57944979+mandro98@users.noreply.github.com>
2022-01-12 15:33:43 +0100

    Update ExceptionTest.java

--- test/ExceptionTest.java
+++ test/ExceptionTest.java
@@ -5,24 +5,37 @@ import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.function.Executable;
 
+import com.currencylayer.exception.AmountFormatException;
 import com.currencylayer.exception.CurrencyNotFoundException;
+import com.currencylayer.parse.JSONParser;
 
 class ExceptionTest {
+	
 
-	@BeforeEach
-	void setUp() throws Exception {
-		Currency c=new Currency();
+	
+	@Test
+	void test2() {
+		Conversion c=new Conversion();
+		AmountFormatException exception = assertThrows(AmountFormatException.class, ()->{c.conversion ("GBP", "USD", -1);
+		});
 		
-	}
-
-	@AfterEach
-	void tearDown() throws Exception {
+		assertEquals("Inserisci un valore positivo", exception.getMessage());
 	}
 
 	@Test
 	void test() {
-		assertThrows(CurrencyNotFoundException.class, ()-> {c.validate("EUY")} );
+		
+		JSONParser j= new JSONParser();
+		
+		CurrencyNotFoundException exception = assertThrows(CurrencyNotFoundException.class, ()->{j.getValuefromApi("EUT");
+		});
+		
+		assertEquals("Errore: La valuta inserita non esiste", exception.getMessage());
 	}
+	
+	
+
 
 }
