c28547e2e0a16c55d77d176add55079f21f16d1f
Mario maio <57944979+mandro98@users.noreply.github.com>
2022-01-12 15:32:51 +0100

    Directory Test JUNIT5

---
+++ test/ExceptionTest.java
@@ -0,0 +1,28 @@
+package com.currencylayer;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import com.currencylayer.exception.CurrencyNotFoundException;
+
+class ExceptionTest {
+
+	@BeforeEach
+	void setUp() throws Exception {
+		Currency c=new Currency();
+		
+	}
+
+	@AfterEach
+	void tearDown() throws Exception {
+	}
+
+	@Test
+	void test() {
+		assertThrows(CurrencyNotFoundException.class, ()-> {c.validate("EUY")} );
+	}
+
+}
