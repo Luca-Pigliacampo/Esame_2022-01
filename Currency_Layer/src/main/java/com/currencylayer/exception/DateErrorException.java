9531bad8d685cfe07c4e1d54f1dd0156c8e82aa5
Mario maio <57944979+mandro98@users.noreply.github.com>
2022-01-17 11:24:22 +0100

    Update DateErrorException.java

--- Currency_Layer/src/main/java/com/currencylayer/exception/DateErrorException.java
+++ Currency_Layer/src/main/java/com/currencylayer/exception/DateErrorException.java
@@ -1,5 +1,16 @@
 package com.currencylayer.exception;
 
+/**
+ * 
+ * @author Mario Maio
+ * @author Emanuele Partemi
+ * @author Luca Pigliacampo
+ * 
+ * This class extends RunTimeExceptions. It is thrown when 
+ * some errors in the format date (YYYY-MM-DD) occure.
+ * Handled by Handler.java
+ */
+
 public class DateErrorException extends RuntimeException{
 	
 	private static final long serialVersionUID = 2L;
