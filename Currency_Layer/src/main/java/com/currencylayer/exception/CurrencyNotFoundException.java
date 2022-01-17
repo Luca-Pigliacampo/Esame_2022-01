3faffbe365fa6dc0960e9943ad67db35d9bea284
Mario maio <57944979+mandro98@users.noreply.github.com>
2022-01-17 11:21:52 +0100

    Update CurrencyNotFoundException.java

--- Currency_Layer/src/main/java/com/currencylayer/exception/CurrencyNotFoundException.java
+++ Currency_Layer/src/main/java/com/currencylayer/exception/CurrencyNotFoundException.java
@@ -1,6 +1,15 @@
 package com.currencylayer.exception;
 
-
+/**
+ * 
+ * @author Mario Maio
+ * @author Emanuele Partemi
+ * @author Luca Pigliacampo
+ * 
+ * This class extends RunTimeExceptions. It is thrown when 
+ * the parser cannot find the JSONObject "currencies", either in API or file.
+ * Handled by Handler.java
+ */
 
 public class CurrencyNotFoundException extends RuntimeException {
 	
